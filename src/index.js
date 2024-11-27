cf3fed9c1f1c039d00f2f20ca15f1b55512042f4
Valerie325 <morevimgh14@gmail.com>
2024-11-27 09:21:27 +0000

    added java script

---
+++ src/index.js
@@ -0,0 +1,4 @@
+let name= prompt("what is your name");
+ let age= prompt("How old are you");
+ let email= prompt("Enter your email address");
+ alert(`Hi ${name}, you are ${age} years old and we will keep in touch via your email: ${email}.👋🏽👋🏽`)
